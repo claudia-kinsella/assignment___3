e223ba7df710264da7c3b22cfdc7dffff59c4aea
claudiakinsella <claudiakinsella@dhcp-892b9f79.ucd.ie>
2017-03-01 16:43:37 +0000

    If statment to remove whitespace before commas in file

--- claudia/Claudia/main.py
+++ claudia/Claudia/main.py
@@ -33,10 +33,10 @@ def main():
     
     for line in lines:
         
-#         if ' ,' in line:
-#             line.replace(' ,',',')
-#         if ', ' in line:
-#             line.replace(', ',',')
+        if ' ,' in line:
+            line.replace(' ,',',')
+        if ', ' in line:
+            line.replace(', ',',')
         if 'turn on' in line or ' turn on' in line:
             a,b,c,d,e = line.split(" ")
             array.turn_on (c,e)
