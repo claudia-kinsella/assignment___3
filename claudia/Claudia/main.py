1327518d72ac23ad641819c4c36456022aca4737
claudiakinsella <claudiakinsella@dhcp-892b9f79.ucd.ie>
2017-03-01 16:32:27 +0000

    Initial main function built and beginning of class 'led_grid'. Strings included to either read commands from terminal or read directly from a linked file within the code.

--- claudia/Claudia/main.py
+++ claudia/Claudia/main.py
@@ -1,47 +1,57 @@
-#!/usr/bin/python
 import urllib.request
 import os
 import sys
-
+from numpy.core.defchararray import startswith
+from pip._vendor.pyparsing import replaceWith
+ 
 def read_file(filename):
+    """Function to read a file and process the contents"""
     if filename.startswith("http"):
-        fh = urllib.request.urlopen(filename)
-        file_str = fh.read().decode("utf-8")
-        return file_str
+        file_h = urllib.request.urlopen(filename)
+        file_line=file_h.read().decode('utf-8')
+        return file_line
     else:
         if not os.path.isfile(filename):
-            print("file does not exist")
+            print('File does not exist.')
         else:
-            file_str = open(filename, "r").read()
-            return file_str
-file1 = read_file(sys.argv[0])       
-# file_str =  
-for line in file1:
-    if line.start("turn on"):
-        line = line.replace(","," ")
-        x1 = line.split([2])
-        y1 = line.split([3])
-        x2 = line.split([5])
-        y2 = line.split([6])
+        # Open the file for reading as a string
+            file_line = open(filename, 'r').read()
+            return file_line 
+            
         
-    elif line.start("turn off"):
-        line = line.replace(","," ")
-        x1 = line.split([2])
-        y1 = line.split([3])
-        x2 = line.split([5])
-        y2 = line.split([6])
-        
-    elif line.start("switch"):
-        line = line.replace(","," ")
-        x1 = line.split([1])
-        y1 = line.split([2])
-        x2 = line.split([4])
-        y2 = line.split([7])
-        
-def turn_on(self, x1, x2, y1, y2):
-    for i in range (y1, y2+1):
-        for j in range (x1, x2+1):
-            self.a2d[i][j] = 1
-        return 
+def main():
+    """Function to make a matrix"""
+#     file_str = read_file('http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3.txt')
+#     first_line = int(file_str.splitlines()[0])
+#     file_line=read_file("../../data/input_assign3.txt")
+#     file_line = read_file('http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3_b_v2.txt')
+    file_line = read_file(sys.argv[2])
+   
+    lines = file_line.split('\n')
+    size = int(lines[0])
+    array = led_grid(size)
     
-
+    for line in lines:
+        
+#         if ' ,' in line:
+#             line.replace(' ,',',')
+#         if ', ' in line:
+#             line.replace(', ',',')
+        if 'turn on' in line or ' turn on' in line:
+            a,b,c,d,e = line.split(" ")
+            array.turn_on (c,e)
+        elif "turn off" in line or ' turn off' in line:
+            a,b,c,d,e = line.split(" ")
+            array.turn_off (c,e)
+        elif "switch" in line or ' switch' in line:
+            a,b,c,d = line.split(" ")
+            array.switch (b,d)
+        else:
+            pass
+    
+class led_grid():
+     
+    def __init__(self, S): 
+        self.size= S 
+        self.grid = [[False]*S for _ in range(S)]
+          
