a97ba67193360e59e7413ff54463c46f5fc59db5
claudiakinsella <claudiakinsella@dhcp-892b9f79.ucd.ie>
2017-03-01 15:58:44 +0000

    Current tests for turn off and turn off need more attention

--- claudia/tests/tests.py
+++ claudia/tests/tests.py
@@ -1,12 +1,31 @@
 from nose.tools import *
 # from src.LEDtester import *
 from Claudia.main import *
-from Claudia.main import read_file
 
 def test_read_url():
     filename ="http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3.txt"
     eq_(read_file(filename)[:4], "1000", "Files don't match")
-    
+     
 def test_read_file():
-    filename = "../data/input_assign3.txt"
+    filename = "../../data/input_assign3.txt"
     eq_(read_file(filename)[:4], "1000", "Files don't match")
+     
+def test_turn_on():
+    ton = led_grid(5)
+    ton.turn_on = ("2,3","3,4")
+    count = 0
+    for i in range(ton.size):
+        for j in range(ton.size):
+            if ton.grid[i][j] == True:
+                count += 1
+    eq_(count,4, 'Turn on does not work')
+    
+def test_turn_off():
+    toff = led_grid(5)
+    toff.turn_off = ("2,3","3,4")
+    count = 0
+    for i in range(toff.size):
+        for j in range(toff.size):
+            if toff.grid[i][j] == True:
+                count += 1
+    eq_(count,4, 'Turn on does not work')
