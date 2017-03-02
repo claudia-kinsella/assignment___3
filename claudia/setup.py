6a35d953e3a1ef8d13042a585dc0aae5f415d04b
claudiakinsella <claudiakinsella@dhcp-892b9f79.ucd.ie>
2017-03-02 16:29:29 +0000

    Setup.py complete

--- claudia/setup.py
+++ claudia/setup.py
@@ -8,12 +8,11 @@ setup(name="LED_Assignment3",
       version="0.1",
       description="Assignment 3",
       url="",
-      
       author="Claudia Kinsella",
       author_email="claudia.kinsella@ucdconnect.ie",
       licence="GPL3",
       packages=['Claudia'],
       entry_points={
-          'console_scripts':['solve_led=claudia.Claudia.main:main']
+          'console_scripts':['solve_led=Claudia.main:main']
           }
       )
