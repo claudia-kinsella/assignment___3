56a62d6b56906a3d38cec81dd49dacd774b7ceda
claudiakinsella <claudiakinsella@dhcp-892b9f79.ucd.ie>
2017-03-02 11:18:50 +0000

    input_range implementation altered

--- claudia/Claudia/main.py
+++ claudia/Claudia/main.py
@@ -63,10 +63,11 @@ class led_grid():
     def turn_off(self, c,e):
         x1,y1 = c.split(',')
         x2,y2 = e.split(',')
-        x1 = int(x1), self.input_range(x1)
-        x2 = int(x2), self.input_range(x2)
-        y1 = int(y1), self.input_range(y1)
-        y2 = int(y2), self.input_range(y2)
+        x1 = int(x1)
+        x2 = int(x2)
+        y1 = int(y1)
+        y2 = int(y2)
+        x1, x2, y1, y2 = self.input_range(x1), self.input_range(x2), self.input_range(y1), self.input_range(y2)
         for i in range (x1,x2+1):
             for j in range (y1, y2+1):
                 self.grid[i][j] = False
@@ -75,10 +76,12 @@ class led_grid():
     def turn_on(self,c,e):
         x1,y1 = c.split(',')
         x2,y2 = e.split(',')
-        x1 = int(x1), self.input_range(x1)
-        x2 = int(x2), self.input_range(x2)
-        y1 = int(y1), self.input_range(x1)
-        y2 = int(y2), self.input_range(y2)
+        x1 = int(x1)
+        x2 = int(x2)
+        y1 = int(y1)
+        y2 = int(y2)
+        x1, x2, y1, y2 = self.input_range(x1), self.input_range(x2), self.input_range(y1), self.input_range(y2)
+
         for i in range (x1,x2+1):
             for j in range (y1, y2+1):
                 self.grid[i][j] = True
@@ -86,10 +89,12 @@ class led_grid():
     def switch(self,b,d):
         x1,y1 = b.split(',')
         x2,y2 = d.split(',')
-        x1 = int(x1), self.input_range(x1)
-        x2 = int(x2), self.input_range(x2)
-        y1 = int(y1), self.input_range(y1)
-        y2 = int(y2), self.input_range(y2)
+        x1 = int(x1)
+        x2 = int(x2)
+        y1 = int(y1)
+        y2 = int(y2)
+        x1, x2, y1, y2 = self.input_range(x1), self.input_range(x2), self.input_range(y1), self.input_range(y2)
+
         for i in range (x1,x2+1):
             for j in range (y1, y2+1):
                 if self.grid[i][j] == True:
