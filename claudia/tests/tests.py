c9e919b0d589bca340421a607968107ba35b244a
claudiakinsella <claudiakinsella@dhcp-892b9f79.ucd.ie>
2017-03-02 16:28:55 +0000

    tests

--- claudia/tests/tests.py
+++ claudia/tests/tests.py
@@ -5,16 +5,16 @@ from Claudia.main import *
 def test_read_url():
     filename ="http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3.txt"
     eq_(read_file(filename)[:4], "1000", "Files don't match")
-          
+           
 def test_read_file():
     filename = "../../data/input_assign3.txt"
     eq_(read_file(filename)[:4], "1000", "Files don't match")    
-  
    
+    
 def test_turn_on():
     tton=led_grid(5)
     tton.turn_on("2,3","3,4")
-    eq_(tton.counter(),4, "test does not work")
+    eq_(tton.counter(),4, "test does not work")  
   
 def test_turn_off():
     ttoff=led_grid(5)
@@ -25,8 +25,8 @@ def test_turn_off():
       
 def test_switch():
     tswitch=led_grid(5)
-    tswitch.turn_on("0,4","0,4")
-    tswitch.switch("2,2", "3,2")
-    eq_(tswitch.counter(),2, 'test does not work')
+    tswitch.turn_on("0,0","4,4")
+    tswitch.switch("1,1","2,2")
+    eq_(tswitch.counter(),21, 'test does not work')
     
     
